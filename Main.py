f592745285f885e4707957ae2de59273907b8ac2
DK <Dikshyant99@gmail.com>
2023-02-09 16:37:07 -0600

    Initial commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+
+
+
+# negative for revolutionary or positive for reactionary
+# scale of 0-10 based on how passionate? 
+
+
+#input : twitter username -> twitter api to get all their tweets -> pass it through our model to predict revolutionary or reactionary
